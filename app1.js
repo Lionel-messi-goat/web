2d360a6229f11563f93051132f7023b20fe32bdb
SJH0428 <jihozzang0428@gmail.com>
2024-12-04 23:58:42 +0900

    Create app1.js

---
+++ app1.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+app.set('port', 3000);
+app.use((req, res, next) => {
+    res.status(200).send('<h1>Welcome! <br>Hello!<br></h1>');
+
+
+});
+
+app.listen(app.get('port'), () => {
+    console.log('Server listening on port 3000');
+});
