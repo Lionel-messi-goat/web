b9de364ab8c410185e5ef06e04cceb38f6c352be
SJH0428 <jihozzang0428@gmail.com>
2024-12-08 22:50:55 +0900

    Update curring1.js

--- curring1.js
+++ curring1.js
@@ -1,8 +1,10 @@
-function greet(greeting) {
-    return function (name) {
-        console.log(greeting + ", " + name);
-    }
-}
+//function greet(greeting) {
+//    return function (name) {
+//        console.log(greeting + ", " + name);
+//    }
+//}
+
+const greet = greeting => name => console.log(greeting + ", " + name);
 
 let hello = greet('hello');
 hello('jiho');
